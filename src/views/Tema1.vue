58dbcc7b89231ea2bf3614fda57636cdaee72503
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-11-05 17:28:22 -0500

    Ajustes video

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -37,11 +37,11 @@
     figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/vpEj821E4tA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Generalidades del equipaje - almacenamiento y bodegas de equipaje
+      figcaption Video: Generalidades del equipaje: almacenamiento y bodegas de equipaje
     figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/at2UjUULDSo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Generalidades del equipaje - Selección de equipaje
+      figcaption Video: Generalidades del equipaje: selección de equipaje
 
     //todo! ANEXAR VIDEOS UNA VES ESTEN LISTOS
     #t_1_2.titulo-segundo
